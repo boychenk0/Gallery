283f836ec4ded37366dd0b8cebe1ad062095cee1
Jack <boychenk0@ukr.net>
2013-07-18 23:04:32 +0300

    fix

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -37,15 +37,16 @@ class ImagesController < ApplicationController
 
   #subscribe
   def subscribe
-    cat_id = params[:id]
+    #cat_id = params[:id]
+    category = Category.find(params[:id])
     status = false
-    if (@s = Subscribe.where(:category_id=>cat_id, :user_id=>current_user.id)).blank?
-      current_user.subscribes.create(:category_id=>cat_id)
+    if (@s = Subscribe.where(:category_id=>category.id, :user_id=>current_user.id)).blank?
+      current_user.subscribes.create(:category=>category)
       status = true
     else
       @s.destroy_all
       status = false
     end
-    render :json=>{:status=>status, :id=>cat_id}, layout: false
+    render :json=>{:status=>status, :id=>category.id}, layout: false
   end
 end
